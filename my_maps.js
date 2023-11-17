77c8e4d192bd533f510305aa227dbecc97f4bb17
ebhertelard <150758358+ebhertelard@users.noreply.github.com>
2023-11-17 10:37:07 -0600

    Add files via upload

---
+++ my_maps.js
@@ -0,0 +1,54 @@
+/*
+NOTA: AL FINAL DE CADA NIVEL, INICIA EL SIGUIENTE 
+*/
+
+const emojis = {
+  '-': ' ',
+  'O': '🕍',
+  'X': '​💟',
+  'I': '👰',
+  'PLAYER': '​🤵​',
+  'BOMB_COLLISION': '🥶',
+  'GAME_OVER': '😭',
+  'WIN': '🥇',
+  'HEART':'❤️'
+ 
+};
+
+const maps = [];
+maps.push(`
+  XXXXXXXXXX
+  XXXXXXXXXX
+  XXXXXXXXXX
+  XXXXXXXXXX
+  XXXXXXXXXX
+  XXXXXXXXXX
+  XXXXXXXXXX
+  XXXXXXXXXX
+  XXXXXXXXXX
+  O--------I
+`);
+maps.push(`
+  O-----XXXX
+  XXXX--XXXX
+  XX----XXXX
+  X--XXXXXXX
+  X--XX--XXX
+  X--XXX-XXX
+  XX--XX--XX
+  XXX--XX-XX
+  XXXX---IXX
+  XXXXXXXXXX
+  `);
+maps.push(`
+  I-----XXXX
+  XXXXX-XXXX
+  XX----XXXX
+  XX-XXXXXXX
+  XX-----XXX
+  XXXXXX-XXX
+  XX-----XXX
+  XX-XXXXXXX
+  XX-----OXX
+  XXXXXXXXXX
+`);
